af4a9360fa405fb78fab6f16a2f7bacb9cd2e180
Rafael Valverde <rafaverde@msn.com>
2025-01-14 11:12:59 -0300

    External fonts added.

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,33 @@
+import { ComponentProps } from "react"
 import { styled } from "./styles"
+
+export type ButtonProps = {
+  size?: "small" | "big"
+}
 
 export const Button = styled("button", {
   fontFamily: "$default",
-  backgroundColor: "$ignite500",
+  backgroundColor: "$ignite300",
   borderRadius: "$md",
-  padding: "$4",
+  cursor: "pointer",
+  border: 0,
+  color: "$white",
+  fontWeight: "$bold",
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "small",
+  },
 })
