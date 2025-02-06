f3819941c3386e95de890fd0ca35e89a49e3899c
Rafael Valverde <rafaverde@msn.com>
2025-02-06 09:14:33 -0300

    fix: Still fixing project names.

--- packages/docs/eslint.config.js
+++ packages/docs/eslint.config.js
@@ -1,14 +1,14 @@
-import js from "@eslint/js"
-import globals from "globals"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
     extends: [
-      "@/ignite-ui/eslint-config",
+      "@/rafaverde-ignite-ui/eslint-config",
       js.configs.recommended,
       ...tseslint.configs.recommended,
     ],
@@ -29,4 +29,4 @@ export default tseslint.config(
       ],
     },
   }
-)
+);
